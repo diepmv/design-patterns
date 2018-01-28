7cdf8dadbad2b7a9ff466717228983cb597e514a
Diep <diepmv@rikkeisoft.com>
2018-01-28 14:31:57 +0700

    update singleton

--- singleton.py
+++ singleton.py
@@ -45,10 +45,11 @@ assert x is y
 
 #======================================================================================
 
-class Singleton():
+class Singleton(object):
   __instance = None
-  def __new__(cls, val=None):
+  def __new__(cls, val = None):
     if Singleton.__instance is None:
       Singleton.__instance = object.__new__(cls)
     Singleton.__instance.val = val
     return Singleton.__instance
+
